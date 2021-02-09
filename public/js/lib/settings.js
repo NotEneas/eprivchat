5f925a77af5e60f4cefc5922f2ddb0f894f02973
NotEneas <60105555+NotEneas@users.noreply.github.com>
2021-02-09 19:13:38 +0100

    Update settings.js

--- public/js/lib/settings.js
+++ public/js/lib/settings.js
@@ -1,6 +1,6 @@
 define({
 
-	title: 'Cryptalk - Online',
+	title: 'EnejPrivChat',
 
 	ttl: 600000,
 
@@ -15,7 +15,7 @@ define({
 		'░          ░░   ░ ▒ ▒ ░░  ░░         ░        ░   ▒     ░ ░   ░ ░░ ░  \n' +
 		'░ ░         ░     ░ ░                             ░  ░    ░  ░░  ░    \n' +
 		'░                 ░ ░                                                 \n' +
-		'                                  https://github.com/hexagon/cryptalk \n' +
+		'                                                                      \n' +
 		'                                                                      \n' +
 		' Tip of the day: /help                                                \n' +
 		'----------------------------------------------------------------------' +
